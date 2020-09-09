a20739aabc6ad23bd166d5f29e97c0e2e29b073a
Kay <joe_kayu@yahoo.com>
2020-09-09 11:44:42 +0100

    added files

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-new WOW().init();
+document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector( 
+          "body").style.visibility = "hidden"; 
+        document.querySelector( 
+          "#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector( 
+          "#loader").style.display = "none"; 
+        document.querySelector( 
+          "body").style.visibility = "visible"; 
+
+    } 
+};
+
+// const icons = document.getElementsByTagName("i");
+
+// for (let i = 0; i < icons.length; i++) {
+//   let icon = icons[i];
+//   icon.addEventListener("mouseover", function() {
+//     // this.style.color = "#000fff";
+//     icon.classList.add('blue')
+//   });
+// }
+
+// const logo = document.querySelectorAll('#logo path');
+// console.log(logo);
+
+// for (let i =0; i<logo.length; i++) {
+//     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
+// }
