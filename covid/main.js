d16c5c5d56faf89368ae04024added6334800cef
Kay <joe_kayu@yahoo.com>
2021-01-09 14:56:42 +0100

    .

--- covid/main.js
+++ covid/main.js
@@ -18,4 +18,29 @@ function myFunction() {
 
 window.addEventListener('scroll', myFunction)
 
-console.log('sticky: ', sticky, 'body: ', document.body.scrollTop, window.pageYOffset )
+// console.log('sticky: ', sticky, 'body: ', document.body.scrollTop, window.pageYOffset )
+
+const linkContainer = document.getElementById("linkCont");
+const links = linkContainer.getElementsByClassName("nav-link");
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+// exclude helpline from the list
+// while (i<links.length-1){
+//   links[i].addEventListener("click", function() {
+//   let current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   // if (i === 5){
+//   // this.className += " ";
+//   // } else {
+//     this.className += " active";
+  
+//   // }
+//   });
+//   i++;
+// }
