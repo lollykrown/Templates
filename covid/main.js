071b60b1c07f3ec580cdf211f64c846521e2166c
Kay <joe_kayu@yahoo.com>
2021-01-09 12:39:07 +0100

    .

--- covid/main.js
+++ covid/main.js
@@ -1,3 +1,21 @@
 // function hideIcon(self) {
 //     self.style.backgroundImage = 'none';
 // }
+
+// window.onscroll = function() {myFunction()};
+
+const header = document.getElementById("header");
+const sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+window.addEventListener('scroll', myFunction)
+
+console.log('sticky: ', sticky, 'body: ', document.body.scrollTop, window.pageYOffset )
