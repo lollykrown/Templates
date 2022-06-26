9e7b18548f27bf2481d970697bb07671a0886339
Kay <joe_kayu@yahoo.com>
2022-06-26 23:26:30 +0100

    tweak

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ function myFunction() {
 
 window.addEventListener('scroll', myFunction)
 
-// console.log('sticky: ', sticky, 'body: ', document.body.scrollTop, window.pageYOffset )
-
 const linkContainer = document.getElementById("linkCont");
 const links = linkContainer.getElementsByClassName("nav-link");
 for (let i = 0; i < links.length; i++) {
@@ -23,18 +21,3 @@ for (let i = 0; i < links.length; i++) {
   this.className += " active";
   });
 }
-
-// exclude helpline from the list
-// while (i<links.length-1){
-//   links[i].addEventListener("click", function() {
-//   let current = document.getElementsByClassName("active");
-//   current[0].className = current[0].className.replace(" active", "");
-//   // if (i === 5){
-//   // this.className += " ";
-//   // } else {
-//     this.className += " active";
-  
-//   // }
-//   });
-//   i++;
-// }
